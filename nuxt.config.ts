7c2e591d58d3d1bd92c5f93bf2c69ee935fa8c20
Stritixx <stritixxy@gmail.com>
2025-09-10 18:57:55 +0200

    Nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
   	compatibilityDate: '2025-07-15',
   	devtools: { enabled: false },
 	components: true,
-	ssr: true,
+	ssr: false,
 	
   	modules: ['@nuxt/eslint'],
 	vite: {
